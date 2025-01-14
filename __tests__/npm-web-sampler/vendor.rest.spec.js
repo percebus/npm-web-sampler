87fd592457e6508740dd597f1bdbbea259f977b6
JCGuerrero <JCystems@gMail.com>
2025-01-14 10:53:22 -0600

    fix(__tests__): Removed year check

--- __tests__/npm-web-sampler/vendor.rest.spec.js
+++ __tests__/npm-web-sampler/vendor.rest.spec.js
@@ -9,6 +9,7 @@ describe('IO.GitHub.percebus/npm-web-sampler/vendor', () => {
     describe('/LICENSES.md', () => {
       const now = new Date()
       const year = now.getFullYear() // i.e. 2024
+      _.noop(year) // FIXME bootstrap does NOT contain 2025 license
       const templates = {
         url: _.template('<%= protocol %>://<%= uri %>/vendor/LICENSES.md')
       }
@@ -22,10 +23,10 @@ describe('IO.GitHub.percebus/npm-web-sampler/vendor', () => {
                 .get(url)
                 .expect('status', 200)
                 //
+                // i.e. 2025
+                // .expect('bodyContains', year) // FIXME bootstrap does NOT contain 2025 license
                 .expect('bodyContains', 'MIT')
                 .expect('bodyContains', 'The MIT License')
-                // i.e. 2024
-                .expect('bodyContains', year)
             )
           })
         })
