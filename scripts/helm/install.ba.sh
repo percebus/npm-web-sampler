b1d7014fd5c0edbbcc75a5f582af290031a42657
percebus <5483088+percebus@users.noreply.github.com>
2025-09-10 20:33:49 +0000

    [MegaLinter] Apply linters fixes

--- scripts/helm/install.ba.sh
+++ scripts/helm/install.ba.sh
@@ -6,18 +6,18 @@ release_name=$1
 
 errors=0
 if [ -z "${release_name}" ]; then
-  echo "Error: release_name is required"
-  errors=$((errors + 1))
+	echo "Error: release_name is required"
+	errors=$((errors + 1))
 fi
 
 if [ -z "${HELM_NAMESPACE}" ]; then
-  echo "Error: HELM_NAMESPACE is required"
-  errors=$((errors + 1))
+	echo "Error: HELM_NAMESPACE is required"
+	errors=$((errors + 1))
 fi
 
 if [ $errors -gt 0 ]; then
-  echo "Got $errors errors! exiting..."
-  exit 1
+	echo "Got $errors errors! exiting..."
+	exit 1
 fi
 
 set -x
--- scripts/helm/uninstall.ba.sh
+++ scripts/helm/uninstall.ba.sh
@@ -6,13 +6,13 @@ release_name=$1
 
 errors=0
 if [ -z "${release_name}" ]; then
-  echo "Error: release_name is required"
-  errors=$((errors + 1))
+	echo "Error: release_name is required"
+	errors=$((errors + 1))
 fi
 
 if [ $errors -gt 0 ]; then
-  echo "Got $errors errors! exiting..."
-  exit 1
+	echo "Got $errors errors! exiting..."
+	exit 1
 fi
 
 set -x
