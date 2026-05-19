f98fe1021b46632509f5a0cf04dc7f03f6f308cf
percebus <5483088+percebus@users.noreply.github.com>
2026-05-19 11:58:04 +0000

    [MegaLinter] Apply linters fixes

--- scripts/clean.ba.sh
+++ scripts/clean.ba.sh
@@ -4,9 +4,9 @@ set -e
 set -v
 
 find ./ -type f \
-  -not -path "./.git/*" \
-  -not -path "./repositories/percebus-assets/*" \
-  -exec sed -i 's/\r$//' {} \;
+	-not -path "./.git/*" \
+	-not -path "./repositories/percebus-assets/*" \
+	-exec sed -i 's/\r$//' {} \;
 
 set +v
 set +e
