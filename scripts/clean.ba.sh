f797d16118fa4ebeeba3f3e06c70fc4ba2841295
percebus <5483088+percebus@users.noreply.github.com>
2026-05-26 17:50:19 +0000

    [MegaLinter] Apply linters fixes

--- scripts/clean.ba.sh
+++ scripts/clean.ba.sh
@@ -5,10 +5,10 @@ set -v
 
 find ./ -type f \
 	\( \
-		-path "./.venv/*" \
-		-o -path "repositories/commons/scripts/*" \
-		-o -path "./scripts/*" \
-		-o -path "./_scripts/*" \
+	-path "./.venv/*" \
+	-o -path "repositories/commons/scripts/*" \
+	-o -path "./scripts/*" \
+	-o -path "./_scripts/*" \
 	\) \
 	-exec sed -i 's/\r$//' {} \;
 
